c0abd93c835cf6e6f49ed550d2f8da7a5c448001
Sumin Kim <61191808+suminkimalff@users.noreply.github.com>
2020-05-01 21:05:11 +0200

    CHANGE Page"Patient"

--- WpfHospitalManagement/Patient.cs
+++ WpfHospitalManagement/Patient.cs
@@ -10,10 +10,40 @@ namespace WpfHospitalManagement
     class Patient
     {
         Grid page;
+        Button buttonNew;
+        Button buttonManagement;
 
         public Patient(Grid page)
         {
             this.page = page;
+            buttonNew = new Button();
+            buttonNew.Height = 100;
+            buttonNew.Width = 100;
+            buttonNew.Content = "New Patient";
+            buttonNew.Margin = new Thickness(70, 155, 580, 164);
+            buttonNew.BorderThickness = new Thickness(0);
+            buttonNew.Click += new RoutedEventHandler(buttonNew_Click);
+            page.Children.Add(buttonNew);
+
+            buttonManagement = new Button();
+            buttonManagement.Content = "Manage Patients";
+            buttonManagement.Height = 100;
+            buttonManagement.Width = 100;
+            buttonManagement.Margin = new Thickness(240, 155, 410, 164);
+            buttonManagement.BorderThickness = new Thickness(0);
+            buttonManagement.Click += new RoutedEventHandler(buttonManagement_Click);
+            page.Children.Add(buttonManagement);
+
+        }
+
+        private void buttonNew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonManagement_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
